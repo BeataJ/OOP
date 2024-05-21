a1e875e3b03084a735d6286b569707385a4bf776
Beata <beata@jasinski.com>
2024-05-21 12:46:04 -0700

    price, name Encapsulation

--- item.py
+++ item.py
@@ -10,26 +10,37 @@ class Item:
 
         # Assign to self object
         self.__name = name
-        self.price = price
+        self.__price = price
         self.quantity = quantity
         
         # Action to execute
         Item.all.append(self)
+        
+    @property
+    def price(self):
+        return self.__price 
+       
+    def apply_discount(self):
+        self.__price = self.__price * self.pay_rate
+        
+    def apply_increment(self, increment_value):
+        self.__price = self.__price + self.__price * increment_value    
 
     @property
     # Property Decoretor = Read-Only Attribute
-    def name(self):
+    def name(self): 
         return self.__name
     
     @name.setter
     def name(self,value):
-        self.__name = value
+        if len(value) > 10:
+            raise Exception("The name is to long")
+        else:
+            self.__name = value
 
     def calculate_total_price(self):
-        return self.price * self.quantity
+        return self.__price * self.quantity
     
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
     
     @classmethod
     def instantiate_form_csv(cls):
@@ -54,6 +65,6 @@ class Item:
             return False      
     
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"             
+        return f"{self.__class__.__name__}('{self.name}', {self.__price}, {self.quantity})"             
 
     
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from item import Item
 
 item1 = Item("MyItem", 750)
 
-item1.name = "otherItem" 
+item1.apply_increment(0.2)
+item1.apply_discount()
 
-print(item1.name)
+print(item1.price)
 
 
 
